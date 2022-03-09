3823a99ea160db25270c5fd5b8c9f96a0f97de18
Kamber Vogli <vogli@in.tum.de>
2022-03-09 02:12:59 +0100

    test scraping with colly

--- appHTTP.go
+++ appHTTP.go
@@ -4,6 +4,8 @@ import (
 	"encoding/csv"
 	"fmt"
 	"net/http"
+	"log"
+	"os"
 	"github.com/gocolly/colly"
 )
 
@@ -31,8 +33,7 @@ func readCSVFromUrl(url string) ([][]string, error) {
 	return make([][]string, 0), nil
 }
 
-func main() {
-	
+func getGraphiteDataFromLRZ() {
 	url := "http://graphite-kom.srv.lrz.de/render/?xFormat=%d.%m.%20%H:%M&tz=CET&from=-5days&target=cactiStyle(group(alias(ap.gesamt.ssid.eduroam,%22eduroam%22),alias(ap.gesamt.ssid.lrz,%22lrz%22),alias(ap.gesamt.ssid.mwn-events,%22mwn-events%22),alias(ap.gesamt.ssid.@BayernWLAN,%22@BayernWLAN%22),alias(ap.gesamt.ssid.other,%22other%22)))&format=csv"
 
 	data, err := readCSVFromUrl(url)
@@ -52,4 +53,34 @@ func main() {
 
 		fmt.Println(row[2])
 	}
+}
+
+func main() {
+	// getGraphiteDataFromLRZ()
+
+	fName := "data.csv"
+	file, err := os.Create(fName)
+
+	if err != nil {
+		log.Fatalf("Could not create file, err: %q", err)
+		return
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	c := colly.NewCollector()
+	c.OnHTML("table#customers", func(e *colly.HTMLElement) {
+		e.ForEach("tr", func(_ int, el *colly.HTMLElement) {
+			writer.Write([]string{
+				el.ChildText("td:nth-child(1)"),
+				el.ChildText("td:nth-child(2)"),
+				el.ChildText("td:nth-child(3)"),
+			})
+		})
+		fmt.Println("Scrapping Complete")
+	})
+	c.Visit("https://www.w3schools.com/html/html_tables.asp")
+
 }
