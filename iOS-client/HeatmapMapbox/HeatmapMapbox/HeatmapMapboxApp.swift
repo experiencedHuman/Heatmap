ad86a53a1fa32c956b3ae1e2f823d10953c2e96c
Kamber Vogli <kamber.vogli@tum.de>
2022-03-15 19:11:24 +0100

    experiment with mapbox sdk

---
+++ iOS-client/HeatmapMapbox/HeatmapMapbox/HeatmapMapboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeatmapMapboxApp.swift
+//  HeatmapMapbox
+//
+//  Created by Kamber Vogli on 15.03.22.
+//
+
+import SwiftUI
+
+@main
+struct HeatmapMapboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
