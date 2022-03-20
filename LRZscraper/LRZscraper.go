c97e3603840bb0888d25fba87999c8c797fffac9
Kamber Vogli <kamber.vogli@tum.de>
2022-03-20 18:42:03 +0100

    store graph data in csv file

--- LRZscraper/LRZscraper.go
+++ LRZscraper/LRZscraper.go
@@ -1,44 +1,99 @@
 package LRZscraper
 
 import (
+	"fmt"
 	"encoding/csv"
-    "fmt"
-    "log"
-    "os"
+	"log"
+	"os"
 	"github.com/gocolly/colly"
 )
 
-func ScrapeData() {
-	fName := "data.csv"
-    file, err := os.Create(fName)
-    if err != nil {
-        log.Fatalf("Could not create file, err: %q", err)
-        return
-    }
-    defer file.Close()
+// It scrapes the table data of a single subdistrict's (Unterbezirk auf Deutsch) page
+// from "https://wlan.lrz.de/apstat/ublist/"
+func ScrapeListOfSubdistricts(fName string) {
+	subdistrictsURL := "https://wlan.lrz.de/apstat/ublist/"
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Could not create file, err: %q", err)
+		return
+	}
+	defer file.Close()
 
-    writer := csv.NewWriter(file)
-    defer writer.Flush()
-	
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
 	c := colly.NewCollector(
 		colly.AllowedDomains("wlan.lrz.de"),
 	)
-	
+
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
-	
-    c.OnHTML("table", func(e *colly.HTMLElement) {
-        e.ForEach("tr", func(_ int, el *colly.HTMLElement) {
-            writer.Write([]string{
-                el.ChildText("td:nth-child(1)"),
-                el.ChildText("td:nth-child(2)"),
-                el.ChildText("td:nth-child(3)"),
-				el.ChildText("td:nth-child(4)"),
-            })
-        })
-        fmt.Println("Scrapping Complete")
-    })
 
-	c.Visit("https://wlan.lrz.de/apstat/ublist/")
+	c.OnError(func(request *colly.Response, err error) {
+	    fmt.Println("Request URL:", request.Request.URL, "failed with response:", request, "\nError:", err)
+	})
+
+	// scrape table's head
+	c.OnHTML("thead", func(e *colly.HTMLElement) {
+		writer.Write([]string{
+			e.ChildText("th:nth-child(1)"),
+			"Link",
+			e.ChildText("th:nth-child(2)"),
+			e.ChildText("th:nth-child(3)"),
+			e.ChildText("th:nth-child(4)"),
+		})
+	})
+
+	// scrape table's body
+	c.OnHTML("tbody", func(e *colly.HTMLElement) {
+		e.ForEach("tr", func(_ int, el *colly.HTMLElement) {
+			writer.Write([]string{
+				el.ChildText("td:nth-child(1)"),
+				el.ChildAttr("a", "href"), // the link of the address
+				el.ChildText("td:nth-child(2)"),
+				el.ChildText("td:nth-child(3)"),
+				el.ChildText("td:nth-child(4)"),
+			})
+		})
+	})
+
+	c.Limit(&colly.LimitRule{DomainGlob: "wlan.lrz.de", Parallelism: 2})
+	error := c.Visit(subdistrictsURL)
+	if error != nil {
+		fmt.Println(error)
+	}
+	c.Wait()
+}
+
+func ScrapeOverviewOfAPs(filename string) {
+	overviewURL := "https://wlan.lrz.de/apstat"
+	fName := filename
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Could not create file, err: %q", err)
+		return
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("wlan.lrz.de"),
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.OnHTML("tbody > tr", func(e *colly.HTMLElement) {
+		selection := e.DOM.Find("td:first-child > a[href]").Text()
+		// ss := fmt.Sprintf("%d", selection)
+		writer.Write([]string{
+			selection,
+		})
+	})
+
+	c.Visit(overviewURL)
 }
--- heatmap.go
+++ heatmap.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	"encoding/csv"
+	// "database/sql"
+	_ "github.com/mattn/go-sqlite3"
+
+	"io"
+	"os"
 	"fmt"
+	"log"
+	"strings"
 	"net/http"
+	"encoding/csv"
+
 	"github.com/experiencedHuman/heatmap/LRZscraper"
 )
 
-func readCSVFromUrl(url string) ([][]string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Could not retrieve csv data from URL!")
-		return nil, err
+func readCSVFromUrl(url string) {
+	resp, httpError := http.Get(url)
+	if httpError != nil {
+		fmt.Println("Could not retrieve csv data from URL!", httpError)
+		return
 	} else {
 		fmt.Println("Successfully retrieved data from URL!")
 	}
@@ -19,40 +27,63 @@ func readCSVFromUrl(url string) ([][]string, error) {
 	defer resp.Body.Close()
 	csvReader := csv.NewReader(resp.Body)
 	csvReader.Comma = ','
+
+	fName := "graphData.csv"
+	file, osError := os.Create(fName)
+	if osError != nil {
+		log.Fatalf("Could not create file, err: %q", osError)
+		return
+	}
+	defer file.Close()
+
+	csvWriter := csv.NewWriter(file)
+	csvWriter.Comma = ';'
+	defer csvWriter.Flush()
+
 	var data []string
-	for i := 0; i < 5; i++ {
-		data, err = csvReader.Read()
-		if err != nil {
-			return nil, err
-		} else {
-			fmt.Printf("%+v\n", data)
-		}
-	}
-	return make([][]string, 0), nil
-}
-
-func getGraphiteDataFromLRZ() {
-	url := "http://graphite-kom.srv.lrz.de/render/?xFormat=%d.%m.%20%H:%M&tz=CET&from=-5days&target=cactiStyle(group(alias(ap.gesamt.ssid.eduroam,%22eduroam%22),alias(ap.gesamt.ssid.lrz,%22lrz%22),alias(ap.gesamt.ssid.mwn-events,%22mwn-events%22),alias(ap.gesamt.ssid.@BayernWLAN,%22@BayernWLAN%22),alias(ap.gesamt.ssid.other,%22other%22)))&format=csv"
-
-	data, err := readCSVFromUrl(url)
-	if err != nil {
-		panic(err)
-	}
-
-	for idx, row := range data {
-		// skip header
-		if idx == 0 {
-			continue
-		}
-
-		if idx == 6 {
+	for i := 0; ; i++ {
+		data, httpError = csvReader.Read()
+		if httpError == io.EOF {
 			break
-		}
+		} else if httpError != nil {
+			panic(httpError)
+			break
+		} else {
+			fields := strings.Fields(data[0]) // get substrings separated by whitespaces
+			name 	:= fields[0]
+			current := strings.Split(fields[1], ":")[1]
+			max 	:= strings.Split(fields[2], ":")[1]
+			min 	:= strings.Split(fields[3], ":")[1]
 
-		fmt.Println(row[2])
+			dateAndTime := data[1]
+			other := data[2] // TODO find out what other is
+			csvWriter.Write([]string{
+				name, current, max, min, dateAndTime, other,
+			})
+		}
 	}
 }
 
 func main() {
-	LRZscraper.ScrapeData()
+	// LRZscraper.ScrapeListOfSubdistricts("subdistricts.csv")
+	LRZscraper.ScrapeOverviewOfAPs("overview.csv")
+	
+	// url := "http://graphite-kom.srv.lrz.de/render/?xFormat=%d.%m.%20%H:%M&tz=CET&from=-2days&target=cactiStyle(group(alias(ap.gesamt.ssid.eduroam,%22eduroam%22),alias(ap.gesamt.ssid.lrz,%22lrz%22),alias(ap.gesamt.ssid.mwn-events,%22mwn-events%22),alias(ap.gesamt.ssid.@BayernWLAN,%22@BayernWLAN%22),alias(ap.gesamt.ssid.other,%22other%22)))&format=csv"
+	// readCSVFromUrl(url)
+
+	// // open a local database instance, located in this directory
+	// db, _ := sql.Open("sqlite3", "./accesspoints.db")
+
+	// stmt, _ := db.Prepare(`
+	// 	INSERT INTO accesspoints (address) values (?)
+	// `)
+
+	// // stmt, _ := db.Prepare(`
+	// // 	CREATE TABLE IF NOT EXISTS "accesspoints" (
+	// // 		"ID"	INTEGER NOT NULL PRIMARY KEY,
+	// // 		"address"	TEXT
+	// // 	);
+	// // `)
+
+	// stmt.Exec("Garching")
 }
