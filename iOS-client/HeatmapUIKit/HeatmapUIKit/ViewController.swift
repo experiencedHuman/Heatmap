10f2f045af57d8f947c1da71500ce80689389cf8
Kamber Vogli <kamber.vogli@tum.de>
2022-04-17 16:51:19 +0200

    use a different heatmap layer for each zoom level from 1-23 and set radius same as zoom level

--- iOS-client/HeatmapUIKit/HeatmapUIKit/ViewController.swift
+++ iOS-client/HeatmapUIKit/HeatmapUIKit/ViewController.swift
@@ -10,7 +10,7 @@ import AzureMapsControl
 
 class ViewController: UIViewController, AzureMapDelegate {
     private var azureMap: MapControl!
-    private var heatmapLayer: HeatMapLayer!
+    private var heatmapLayers: [HeatMapLayer?]!
     private var radius = 20.0, opacity = 0.8, intensity = 1.0, weight = 1.0
     
     private var radiusLabel  = UILabel(),
@@ -18,14 +18,11 @@ class ViewController: UIViewController, AzureMapDelegate {
                 intensityLabel = UILabel(),
                 weightLabel    = UILabel()
     
-    func azureMapCameraIsMoving(_ map: AzureMap) {
-        // TODO use NSExpression with Interpolation expression since i cannot access zoomLevel
-        // use interpolation for making the heatmap smaller while zooming out :(
-        
-        //Option 2: PLay with minZoom, maxZoom and extra heatmap layers which we can add on the azureMap based on the min/maxZoom and visibility options
-        
-        // Option 3: Request feature as Github issue
-        // github issue for subscription key being wrong, google maps for radius .clearTileCache() not working
+    private var dataSource: DataSource!
+    private var heatmapLayer: HeatMapLayer!
+    
+    func azureMap(_ map: AzureMap, didTapAt location: CLLocationCoordinate2D) {
+        heatmapLayer = heatmapLayers[0]
     }
     
     override func loadView() {
@@ -34,61 +31,46 @@ class ViewController: UIViewController, AzureMapDelegate {
                                    options: [
                                     CameraOption.center(lat: 48.2692083204, lng: 11.6690079838),
                                     CameraOption.zoom(9),
-                                    // StyleOption.style(.grayscaleDark)
                                    ])
+        setupDataSource()
+        
+        azureMap.onReady { map in
+            map.sources.add(self.dataSource)
+            
+            //setup heatmap layers and add them all to map
+            self.heatmapLayers = [HeatMapLayer?](repeating: nil, count: 23)
+            for i in 1...23 {
+                let heatmapLayer = HeatMapLayer(
+                    source: self.dataSource,
+                    options: [
+                        .heatmapRadius(10.0 + Double(i)),
+                        .heatmapOpacity(0.8),
+                        .minZoom(Double(i) - 0.1),
+                        .maxZoom(Double(i) + 0.9)
+                    ]
+                )
+                self.heatmapLayers[i - 1] = heatmapLayer
+                map.layers.insertLayer(heatmapLayer, below: "labels")
+            }
+        }
         
         self.view.addSubview(azureMap)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let source = DataSource()
-        let locations = readCoordsFromJSON(file: "accessPoints")
-        let pointCollection = PointCollection(locations)
-        source.add(geometry: pointCollection)
-        
-        azureMap.onReady { map in
-            map.sources.add(source)
-            self.heatmapLayer = HeatMapLayer(source: source, options: [
-                .heatmapRadius(from: NSExpression(
-                    forAZMInterpolating: .zoomLevelAZMVariable,
-                    curveType: ExpressionInterpolationMode.exponential,
-                    parameters: NSExpression(forConstantValue: 2),
-                    stops: NSExpression(forConstantValue: [
-                        
-                        // For zoom level 1 set the radius to 2 points.
-                        1: 10,
-                        
-                        // Between zoom level 1 and 19, exponentially scale the radius from 2 points to 2 * 2^(maxZoom - minZoom) points.
-                        22: 100
-                    ])
-                )),
-                .heatmapOpacity(0.8),
-                //                .heatmapColor(
-                //                    from: // Stepped color expression
-                //                            NSExpression(
-                //                                forAZMStepping: .heatmapDensityAZMVariable,
-                //                                from: NSExpression(forConstantValue: UIColor.clear),
-                //                                stops: NSExpression(forConstantValue: [
-                //                                    0.01: UIColor(red: 0, green: 0, blue: 128/255, alpha: 1),
-                //                                    0.25: UIColor.cyan,
-                //                                    0.5: UIColor.green,
-                //                                    0.75: UIColor.yellow,
-                //                                    1: UIColor.red
-                //                                ])
-                //                            )
-                //                ),
-                    .minZoom(5.0)
-            ])
-            
-            map.layers.insertLayer(self.heatmapLayer, below: "labels")
-        }
-        
+        // Add slider controls after loading the view.
         addControl(title: "Radius",  valueLabel: &radiusLabel,  distFromTop: 50,  min: 1, max: 50, #selector(updateRadius(_:)))
         addControl(title: "Opacity", valueLabel: &opacityLabel, distFromTop: 100, min: 0, max: 1,  #selector(updateOpacity(_:)))
         addControl(title: "Intencity", valueLabel: &intensityLabel, distFromTop: 150, min: 0, max: 5,  #selector(updateIntensity(_:)))
         addControl(title: "Weight", valueLabel: &weightLabel, distFromTop: 200, min: 0, max: 5,  #selector(updateWeight(_:)))
+    }
+    
+    private func setupDataSource() {
+        dataSource = DataSource()
+        let locations = readCoordsFromJSON(file: "accessPoints")
+        let pointCollection = PointCollection(locations)
+        dataSource.add(geometry: pointCollection)
     }
     
     private func addControl(title: String, valueLabel: inout UILabel, distFromTop: Int, min: Float, max: Float ,_ objcFunc: Selector) {
