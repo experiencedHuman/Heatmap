04ac2f92d9a7096891170032002c8a529d33f5d6
Kamber Vogli <kamber.vogli@tum.de>
2022-04-17 15:44:34 +0200

    added sliders for intencity and weight properties of the heatmap

--- iOS-client/HeatmapUIKit/HeatmapUIKit/ViewController.swift
+++ iOS-client/HeatmapUIKit/HeatmapUIKit/ViewController.swift
@@ -8,21 +8,35 @@
 import UIKit
 import AzureMapsControl
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AzureMapDelegate {
     private var azureMap: MapControl!
     private var heatmapLayer: HeatMapLayer!
-    private var radius: Double = 20, opacity: Double = 0.8
+    private var radius = 20.0, opacity = 0.8, intensity = 1.0, weight = 1.0
     
-    private var radiusLabel, opacityLabel: UILabel!
+    private var radiusLabel  = UILabel(),
+                opacityLabel = UILabel(),
+                intensityLabel = UILabel(),
+                weightLabel    = UILabel()
+    
+    func azureMapCameraIsMoving(_ map: AzureMap) {
+        // TODO use NSExpression with Interpolation expression since i cannot access zoomLevel
+        // use interpolation for making the heatmap smaller while zooming out :(
+        
+        //Option 2: PLay with minZoom, maxZoom and extra heatmap layers which we can add on the azureMap based on the min/maxZoom and visibility options
+        
+        // Option 3: Request feature as Github issue
+        // github issue for subscription key being wrong, google maps for radius .clearTileCache() not working
+    }
     
     override func loadView() {
         super.loadView()
         azureMap = MapControl.init(frame: CGRect(x: 0, y: 0, width: 500, height: 800),
-                                options: [
+                                   options: [
                                     CameraOption.center(lat: 48.2692083204, lng: 11.6690079838),
                                     CameraOption.zoom(9),
-//                                    StyleOption.style(.grayscaleDark)
-                                ])
+                                    // StyleOption.style(.grayscaleDark)
+                                   ])
+        
         self.view.addSubview(azureMap)
     }
     
@@ -42,89 +56,100 @@ class ViewController: UIViewController {
                     curveType: ExpressionInterpolationMode.exponential,
                     parameters: NSExpression(forConstantValue: 2),
                     stops: NSExpression(forConstantValue: [
-
+                        
                         // For zoom level 1 set the radius to 2 points.
-                        1: 5,
-
+                        1: 10,
+                        
                         // Between zoom level 1 and 19, exponentially scale the radius from 2 points to 2 * 2^(maxZoom - minZoom) points.
-                        22: 500
+                        22: 100
                     ])
                 )),
                 .heatmapOpacity(0.8),
-//                .heatmapColor(
-//                    from: // Stepped color expression
-//                            NSExpression(
-//                                forAZMStepping: .heatmapDensityAZMVariable,
-//                                from: NSExpression(forConstantValue: UIColor.clear),
-//                                stops: NSExpression(forConstantValue: [
-//                                    0.01: UIColor(red: 0, green: 0, blue: 128/255, alpha: 1),
-//                                    0.25: UIColor.cyan,
-//                                    0.5: UIColor.green,
-//                                    0.75: UIColor.yellow,
-//                                    1: UIColor.red
-//                                ])
-//                            )
-//                ),
-                .minZoom(5.0)
+                //                .heatmapColor(
+                //                    from: // Stepped color expression
+                //                            NSExpression(
+                //                                forAZMStepping: .heatmapDensityAZMVariable,
+                //                                from: NSExpression(forConstantValue: UIColor.clear),
+                //                                stops: NSExpression(forConstantValue: [
+                //                                    0.01: UIColor(red: 0, green: 0, blue: 128/255, alpha: 1),
+                //                                    0.25: UIColor.cyan,
+                //                                    0.5: UIColor.green,
+                //                                    0.75: UIColor.yellow,
+                //                                    1: UIColor.red
+                //                                ])
+                //                            )
+                //                ),
+                    .minZoom(5.0)
             ])
             
             map.layers.insertLayer(self.heatmapLayer, below: "labels")
         }
-        addRadiusControl()
-        addOpacityControl()
+        
+        addControl(title: "Radius",  valueLabel: &radiusLabel,  distFromTop: 50,  min: 1, max: 50, #selector(updateRadius(_:)))
+        addControl(title: "Opacity", valueLabel: &opacityLabel, distFromTop: 100, min: 0, max: 1,  #selector(updateOpacity(_:)))
+        addControl(title: "Intencity", valueLabel: &intensityLabel, distFromTop: 150, min: 0, max: 5,  #selector(updateIntensity(_:)))
+        addControl(title: "Weight", valueLabel: &weightLabel, distFromTop: 200, min: 0, max: 5,  #selector(updateWeight(_:)))
     }
     
-    private func addOpacityControl() {
-        let radiusTitle = UILabel(frame: CGRect(x: 510, y: 50, width: 80, height: 20))
-        radiusTitle.text = "Radius"
-        addUISlider(xOrig: 600, yOrig: 50, width: 200, height: 20, minVal: 1, maxVal: 50, objcFunc: #selector(radiusSliderValueDidChange(_:)))
-        radiusLabel = UILabel(frame: CGRect(x: 850, y: 50, width: 100, height: 20))
-        self.view.addSubview(radiusTitle)
-        radiusLabel.text = "0.0"
-        self.view.addSubview(radiusLabel)
+    private func addControl(title: String, valueLabel: inout UILabel, distFromTop: Int, min: Float, max: Float ,_ objcFunc: Selector) {
+        let titleLabel = UILabel(frame: CGRect(x: 510, y: distFromTop, width: 80, height: 20))
+        titleLabel.text = title
+        valueLabel = UILabel(frame: CGRect(x: 850, y: distFromTop, width: 100, height: 20))
+        valueLabel.text = "0.0"
+        addUISlider(xOrig: 600, yOrig: distFromTop, width: 200, height: 20, minVal: min, maxVal: max, objcFunc: objcFunc)
+        view.addSubview(titleLabel)
+        view.addSubview(valueLabel)
     }
     
-    private func addRadiusControl() {
-        let opacityTitle = UILabel(frame: CGRect(x: 510, y: 100, width: 80, height: 20))
-        opacityTitle.text = "Opacity"
-        addUISlider(xOrig: 600, yOrig: 100, width: 200, height: 20, minVal: 0, maxVal: 1, objcFunc: #selector(opacitySliderValueDidChange(_:)))
-        opacityLabel = UILabel(frame: CGRect(x: 850, y: 100, width: 100, height: 20))
-        self.view.addSubview(opacityTitle)
-        opacityLabel.text = "0.0"
-        self.view.addSubview(opacityLabel)
-    }
-
     private func addUISlider(xOrig: Int, yOrig: Int, width: Int, height: Int, minVal: Float, maxVal: Float, objcFunc: Selector) {
-        let slider = UISlider(frame:CGRect(x: xOrig, y: yOrig, width: width, height: height))
+        let slider = UISlider(frame: CGRect(x: xOrig, y: yOrig, width: width, height: height))
         slider.minimumValue = minVal
         slider.maximumValue = maxVal
         slider.isContinuous = true
         slider.addTarget(self, action: objcFunc, for: .valueChanged)
         view.addSubview(slider)
     }
-
+    
     @objc
-    func radiusSliderValueDidChange(_ sender:UISlider!) {
+    func updateRadius(_ sender: UISlider!) {
         let step:Float = 0.05
         let stepValue = round(sender.value / step) * step
         sender.value = stepValue
         radius = Double(stepValue)
         radiusLabel.text = String(radius)
         heatmapLayer.setOptions([.heatmapRadius(radius)])
-        print("Radius: \(radius)")
     }
     
     @objc
-    func opacitySliderValueDidChange(_ sender:UISlider!) {
-        let step:Float = 0.05
+    func updateOpacity(_ sender: UISlider!) {
+        let step:Float = 0.01
         let stepValue = round(sender.value / step) * step
         sender.value = stepValue
         opacity = Double(stepValue)
         opacityLabel.text = String(opacity)
         heatmapLayer.setOptions([.heatmapOpacity(opacity)])
-        print("Opacity: \(opacity)")
     }
-
+    
+    @objc
+    func updateIntensity(_ sender: UISlider!) {
+        let step:Float = 0.01
+        let stepValue = round(sender.value / step) * step
+        sender.value = stepValue
+        intensity = Double(stepValue)
+        intensityLabel.text = String(intensity)
+        heatmapLayer.setOptions([.heatmapIntensity(intensity)])
+    }
+    
+    @objc
+    func updateWeight(_ sender: UISlider!) {
+        let step:Float = 0.01
+        let stepValue = round(sender.value / step) * step
+        sender.value = stepValue
+        weight = Double(stepValue)
+        weightLabel.text = String(weight)
+        heatmapLayer.setOptions([.heatmapWeight(weight)])
+    }
+    
     
     private func readCoordsFromJSON(file filename: String) -> [CLLocationCoordinate2D] {
         var coordinates: [CLLocationCoordinate2D] = []
